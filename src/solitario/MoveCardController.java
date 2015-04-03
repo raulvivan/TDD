5b78a5e71f5bc2aea533d99ebefa8db86bdb9c5f
raulvivan <raul.vivanco@opendeusto.es>
2015-04-03 11:06:42 +0200

    Implementados metodos moverBarajaADescarte y getDeckCards

--- src/solitario/MoveCardController.java
+++ src/solitario/MoveCardController.java
@@ -6,6 +6,7 @@ public class MoveCardController {
 	
 	private Stack<Card> cardsDeckStack;
 	private Stack<Card> cardsWasteStack;
+	private final int CARTASROBAR = 3;
 
 	public MoveCardController(Stack<Card> baraja, Stack<Card> descartes) {
 		this.cardsDeckStack = baraja;
@@ -17,13 +18,24 @@ public class MoveCardController {
 	}
 
 	public void moverBarajaADescarte() {
-		// TODO Auto-generated method stub
+		if(cardsDeckStack.size() >= CARTASROBAR){
+			for(int i = 0; i<CARTASROBAR; i++){
+				Card card = cardsDeckStack.pop();
+				card.flip();
+				cardsWasteStack.push(card);
+			}
+		}else{
+			for(int i = 0; i<cardsDeckStack.size(); i++){
+				Card card = cardsDeckStack.pop();
+				card.flip();
+				cardsWasteStack.push(card);
+			}
+		}
 		
 	}
 
 	public Stack<Card> getDeckCards() {
-		// TODO Auto-generated method stub
-		return null;
+		return cardsDeckStack;
 	}
 
 }
--- src/test/MoveCardControllerTest.java
+++ src/test/MoveCardControllerTest.java
@@ -31,14 +31,17 @@ public class MoveCardControllerTest {
 		Stack<Card> wasteCards = moveCardController.getWasteCards();
 
 		assertEquals(3, wasteCards.size());
-		assertTrue(moveCardController.getDeckCards().size() == cartasBaraja-3);
-		assertTrue(moveCardController.getWasteCards().size() == cartasDescartes+3);
-		
+		if(cartasBaraja>=3){
+			assertTrue(moveCardController.getDeckCards().size() == cartasBaraja-3);
+			assertTrue(moveCardController.getWasteCards().size() == cartasDescartes+3);
+		}else{
+			assertTrue(moveCardController.getDeckCards().size() == 0);
+			assertTrue(moveCardController.getWasteCards().size() == cartasDescartes+cartasBaraja);
+		}
 		for (Card card : wasteCards) {
 			assertNotNull(card);
 			assertTrue(card.uncovered());
 			assertTrue(wasteCards.indexOf(card) == wasteCards.lastIndexOf(card));
 		}
 	}
-
 }
